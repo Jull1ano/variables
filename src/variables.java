7931338ca93dc8610d4dfdc62b0a65ad25581c18
Iulian <iulian181@gmail.com>
2019-07-27 11:51:45 +0300

    Initial Commit

---
+++ src/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        String word = "Iulian";
+        int num = 5;
+        num = num + 2;
+        int num2 = 20;
+        //System.out.println(word + " " + num + 5 + " Art");
+        //System.out.println(num * 2 / 14 + num2);
+        System.out.println(2 + (5 * 10 / 25));
+    }
+}
